0ea36423eb95ce560d3ee5462156096c3a207124
Tanner Lancaster <tanner@flatironschool.com>
2019-01-27 03:41:39 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,6 +15,7 @@ def first_challenge
 
   #your code here
   contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry")
+  puts contacts
 
   #remember to return your newly altered contacts hash!
   contacts
